b8a43d046fbd66d92ba73bb3b28c2634f29d71c5
Devin Schutz <schutzdevin@gmail.com>
2025-10-09 13:42:34 -0600

    Added the logout button in a really janky way. Then made it so the homepage url is login protected again like it used to be.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import DoctorCards from "@/components/cards/doctor-display-cards";
+import { LogoutButton } from "@/components/logout-button";
 
 export default function Home() {
   return (
    <div className="flex flex-col">
       <DoctorCards page={0} />
+      <LogoutButton />
    </div>
   );
 }
