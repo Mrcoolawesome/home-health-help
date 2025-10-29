e0a9d360400d0f51f126eb919bf3e9005a55e3bc
Zane Bennion <zanebennion@gmail.com>
2025-10-29 12:28:37 -0600

    displaying comparisons in a grid layout

--- components/details/popout.tsx
+++ components/details/popout.tsx
@@ -14,7 +14,7 @@ export default function Popout({ description, children }: popoutProps){
       </Popover.Trigger>
       <Popover.Portal>
         <Popover.Positioner>
-          <Popover.Popup className="origin-[var(--transform-origin)] rounded-lg bg-background px-4 py-2 text-foreground outline outline-2 shadow-sm outline-foreground-alt transition-[transform,scale,opacity] data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[starting-style]:scale-90 data-[starting-style]:opacity-0">
+          <Popover.Popup className="origin-[var(--transform-origin)] max-w-[500px] rounded-lg bg-background px-4 py-2 text-foreground outline outline-2 shadow-sm outline-foreground-alt transition-[transform,scale,opacity] data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[starting-style]:scale-90 data-[starting-style]:opacity-0">
             <Popover.Description>{description}</Popover.Description>
           </Popover.Popup>
         </Popover.Positioner>
