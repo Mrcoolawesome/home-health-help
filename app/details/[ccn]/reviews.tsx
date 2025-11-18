716b3eb6617b9a5cf48c29ce288196b7e4d5117b
zanebennion <zanebennion@gmail.com>
2025-11-18 15:20:10 -0700

    ratings are somewhat styled

--- app/details/[ccn]/reviews.tsx
+++ app/details/[ccn]/reviews.tsx
@@ -4,11 +4,16 @@ import { PlaceReview } from "@/lib/types";
 
 export default async function GoogleReviews({ placeID }: { placeID: string }) {
   const reviews = await getNewestReviews(placeID);
+  let sum = 0;
+  reviews?.result.reviews?.map((review: PlaceReview) => sum += review.rating);
+
+  const avgRating = sum / 5;
 
   return (
     <div className="container mx-auto w-full px-4 py-8 space-y-8">
       <section className="rounded-lg border border-foreground-alt bg-background text-foreground p-6">
         <h2 className="text-xl font-semibold mb-4">Reviews</h2>
+        <h2 className="text-xl font-semibold mb-4">Average: {avgRating}</h2>
         {reviews?.result.reviews?.map((review: PlaceReview, index) => (
           <SingleReview review={review} key={index} />
         ))}
--- components/details/singleReview.tsx
+++ components/details/singleReview.tsx
@@ -1,9 +1,20 @@
 import { PlaceReview } from "@/lib/types";
+import { StarSolid } from "iconoir-react";
 
 export default function SingleReview({ review }: { review: PlaceReview }) {
   return (
-    <div>
-      {review.text}
+    <div className="border border-foreground m-2 p-2 rounded">
+      <div className="flex flex-row">
+        <span className="mr-2">{review.author_name}</span>
+        <StarSolid />
+        <span className="mr-2">
+          {review.rating}
+        </span>
+        <span className="text-foreground-alt">{review.relative_time_description}</span>
+      </div>
+      <div>
+        {review.text}
+      </div>
     </div>
   )
 }
