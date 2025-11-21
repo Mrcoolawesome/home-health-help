f85e00d8f198e2db84a21b56d03a3845fdf08f7e
zanebennion <zanebennion@gmail.com>
2025-11-21 12:23:33 -0700

    details and compare page working with global variables

--- lib/hospice-data/national-data.ts
+++ lib/hospice-data/national-data.ts
@@ -2,40 +2,41 @@ import { RawNationalDataRecord } from "@/lib/types";
 
 export async function getNationalData(): Promise<RawNationalDataRecord[] | null> {
   try {
-      const response = await fetch(`https://data.cms.gov/provider-data/api/1/datastore/sql?query=%5BSELECT%20%2A%20FROM%2061a6e76d-a020-59f1-be5e-8fd77aca5800%5D`);
-  
-      if (!response.ok) {
-        console.error(`HTTP error! status: ${response.status}`);
-        return null;
-      }
-  
-      const data: RawNationalDataRecord[] = await response.json();
-      return data;
+    const response = await fetch(`https://data.cms.gov/provider-data/api/1/datastore/sql?query=%5BSELECT%20%2A%20FROM%20${NATIONAL_DATA}%5D`);
 
-    } catch (error) {
-      // Handle network errors or other exceptions
-      console.error("Failed to fetch national data:", error);
+    if (!response.ok) {
+      console.error(`HTTP error! status: ${response.status}`);
       return null;
     }
+
+    const data: RawNationalDataRecord[] = await response.json();
+    return data;
+
+  } catch (error) {
+    // Handle network errors or other exceptions
+    console.error("Failed to fetch national data:", error);
+    return null;
+  }
 }
 
 import { RawNationalCahpsRecord } from "@/lib/types";
+import { NATIONAL_CAHPS_DATA, NATIONAL_DATA } from "../globals";
 
 export async function getNationalCahps(): Promise<RawNationalCahpsRecord[] | null> {
   try {
-      const response = await fetch(`https://data.cms.gov/provider-data/api/1/datastore/sql?query=%5BSELECT%20%2A%20FROM%20cec9c15f-8712-5f08-ac05-918121f7c54d%5D`);
-  
-      if (!response.ok) {
-        console.error(`HTTP error! status: ${response.status}`);
-        return null;
-      }
-  
-      const data: RawNationalCahpsRecord[] = await response.json();
-      return data;
+    const response = await fetch(`https://data.cms.gov/provider-data/api/1/datastore/sql?query=%5BSELECT%20%2A%20FROM%20${NATIONAL_CAHPS_DATA}%5D`);
 
-    } catch (error) {
-      // Handle network errors or other exceptions
-      console.error("Failed to fetch national CAHPS data:", error);
+    if (!response.ok) {
+      console.error(`HTTP error! status: ${response.status}`);
       return null;
     }
+
+    const data: RawNationalCahpsRecord[] = await response.json();
+    return data;
+
+  } catch (error) {
+    // Handle network errors or other exceptions
+    console.error("Failed to fetch national CAHPS data:", error);
+    return null;
+  }
 }
